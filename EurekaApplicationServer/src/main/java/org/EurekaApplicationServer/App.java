e99932cc8fbdeb783c3f78896ca07638999f22a4
Parthasarathy G <691918@cognizant.com>
2020-03-02 13:16:31 +0530

    adding get instance list from spring cloud endpoint

--- EurekaApplicationServer/src/main/java/org/EurekaApplicationServer/App.java
+++ EurekaApplicationServer/src/main/java/org/EurekaApplicationServer/App.java
@@ -1,8 +1,17 @@
 package org.EurekaApplicationServer;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.netflix.appinfo.InstanceInfo;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -11,4 +20,5 @@ public class App
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+	
 }
---
+++ EurekaApplicationServer/src/main/java/org/EurekaApplicationServer/ServiceInstanceInfoClient.java
@@ -0,0 +1,34 @@
+package org.EurekaApplicationServer;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceInstanceInfoClient {
+	@Autowired
+	DiscoveryClient discoveryClient;
+	
+	@RequestMapping("/service-instances/{service}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+	        @PathVariable String service) {
+	     /*List<ServiceInstance> instanceInfo= this.discoveryClient.get(service);
+	    String appNameString=null;
+	    System.out.println("service looking for " + service +instanceInfo.get(0).toString());
+	     for (ServiceInstance serviceInstance : instanceInfo) {
+	    	 appNameString=serviceInstance.getHost()+serviceInstance.getPort();
+	    	 System.out.println("Application name" + appNameString);
+		}
+		
+	     return appNameString;
+	     */
+		 System.out.println("service name" + service);
+		return this.discoveryClient.getInstances( service);
+	     
+	}
+}
